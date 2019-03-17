cc0da587f2b80d70b49b350b900f0d1cb13441f5
Marcal Ramos <suncastelo@gmail.com>
2019-03-17 06:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // write your code below!
 var name = "Susan"
 var height = 30
-var message = ` is ${height} inches tall`
-
+var message = ` is ${height} inches tall ${name} `
 module.exports = {name, height, message}
